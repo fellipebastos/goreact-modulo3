c2db7ea75a596cc309d502874e56266ba7ea2ac0
fellipebastos <fellipebastoswm@gmail.com>
2019-07-20 13:29:58 -0300

    install and configure redux and provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Provider store={store}>
+        <h1>Hello Word</h1>
+      </Provider>
     </div>
   );
 }
